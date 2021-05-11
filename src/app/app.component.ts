df71545d147dbc58274e29303fa621c20d591aef
tunirsaha <sahatunir@gmail.com>
2021-05-11 21:25:00 +0530

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Tunir Saha'
+  dogCount = 1
+  dogs = [
+    {
+      name: "Jimmy",
+      activity: [
+        "walk",
+        "eat",
+        "bite people"
+      ],
+      image: "https://th.bing.com/th/id/OIP.EkOxo19lVUaq6eVlVPxZ2wHaE7?w=239&h=180&c=7&o=5&dpr=1.25&pid=1.7"
+    },
+    {
+      name: "Tommy",
+      activity: [
+        "sleep",
+        "eat",
+        "swim"
+      ],
+      image: "https://th.bing.com/th/id/OIP.Encpy4zhjNpIb0NAqlbLfwHaE7?w=239&h=180&c=7&o=5&dpr=1.25&pid=1.7"
+    },
+    {
+      name: "Brad",
+      activity: [
+        "walk",
+        "swim",
+        "chase cats"
+      ],
+      image: ""
+    }
+  ]
+  addDog() {
+    this.dogCount += 1
+  }
+}
